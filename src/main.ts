1de2fbc98df2bae06f8fa0aab1a57e787ab8d011
YOUJIN LEE <suddenlydisappear200@gmail.com>
2023-07-26 15:42:25 +0900

    add: cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   try {
-    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+      cors: true,
+    });
     await app.listen(3000);
   } catch (err) {
     console.log(err);
