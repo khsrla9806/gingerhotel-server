8bb5bc4607f9405930112efaabccac1ee02371a9
khsrla9806 <kimhunsope@naver.com>
2023-08-23 20:43:54 +0900

    add: 각 제공업체에 해당하는 mutation 메서드 구현 #7
    - 구조적인 틀만 잡아둔 상태입니다.
    - 각 제공업체에서 제공하는 입력 데이터가 아직 불확실해서 임시 구조

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,9 +1,7 @@
-/* eslint-disable */
 import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { UserService } from "./users.service";
 import { User } from "./entities/user.entity";
-import { CreateAccountInput, CreateAccountOutput } from "./dtos/create-account-dto";
-import { LoginInput, LoginOutput } from "./dtos/login.dto";
+import { AppleSocialInput, GoogleSocialInput, KakaoSocialInput, NaverSocialInput, SocialOutput } from "./dtos/social-login-dto";
 
 @Resolver(of => User)
 export class UserResolver {
@@ -11,9 +9,44 @@ export class UserResolver {
       private readonly usersService: UserService
     ) {}
 
+    @Mutation(returns => SocialOutput)
+    async socialKakao(@Args('data') data: KakaoSocialInput) {
+
+      return {
+        ok: true,
+        accessToken: "카카오 사용자"
+      };
     }
 
+    @Mutation(returns => SocialOutput)
+    async socialGoogle(@Args('data') data: GoogleSocialInput) {
+      
+      return {
+        ok: true,
+        accessToken: "구글 사용자"
+      };
     }
 
+    @Mutation(returns => SocialOutput)
+    async socialNaver(@Args('data') data: NaverSocialInput) {
+      
+      return {
+        ok: true,
+        accessToken: "네이버 사용자"
+      };
+    }
+
+    @Mutation(returns => SocialOutput)
+    async socialApple(@Args('data') data: AppleSocialInput) {
+      
+      return {
+        ok: true,
+        accessToken: "애플 사용자"
+      };
+    }
+
+    @Query(returns => Boolean)
+    hi() {
+        return true;
     }
 }
