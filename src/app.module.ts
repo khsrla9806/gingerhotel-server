2d86c3ed8f6ac3dbf5dea7dff12a79dc512990f7
khsrla9806 <kimhunsope@naver.com>
2023-08-29 23:12:01 +0900

    add: jwt 관련 env 설정 후 UserModule 설정 #7
    - PassportModule 등록
    - JwtModule 등록 후 설정 값 등록
    - JwtStrategy providers에 등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,7 +26,9 @@ import * as Joi from 'joi';
         DB_PORT: Joi.string().required(),
         DB_USERNAME: Joi.string().required(),
         DB_PASSWORD: Joi.string().required(),
-        DB_NAME: Joi.string().required()
+        DB_NAME: Joi.string().required(),
+        JWT_SECRET_KEY: Joi.string().required(),
+        ACCESS_TOKEN_EXP: Joi.string().required()
       })
     }),
     TypeOrmModule.forRoot({
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,9 +4,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UserResolver } from './users.resolver';
 import { UserService } from './users.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
-    providers: [UserResolver, UserService],
+    imports: [
+        TypeOrmModule.forFeature([User]),
+        PassportModule,
+        JwtModule.registerAsync({
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+              secret: configService.get('JWT_SECRET_KEY'),
+              signOptions: { expiresIn: configService.get('ACCESS_TOKEN_EXP') },
+            }),
+          }),
+    ],
+    providers: [UserResolver, UserService, JwtStrategy],
 })
-export class UsersModule {}
+export class UserModule {}
