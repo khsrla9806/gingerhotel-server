e928d56a2cfd9fdd3c14594c1bb400bc570cdf05
khsrla9806 <kimhunsope@naver.com>
2023-08-29 21:15:08 +0900

    add: 소셜 로그인, 회원가입 service 메서드 구현 #7
    - 이미 존재하는 회원인지 아닌지 식별 값을 가지고 확인
    - JWT 토큰 기능 아직 추가 되지 않음
    - 로그인 요청으로 들어온 사용자의 DB Vendor 정보와 요청으로 들어온 vendor 정보가 다르면 Error

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,8 +2,9 @@
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { User } from "./entities/user.entity";
-import { Injectable } from '@nestjs/common';
-import { CreateAccountInput } from "./dtos/create-account-dto";
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Vendor } from "./entities/vendor.type";
+import { Gender } from "./entities/gender.type";
 
 @Injectable()
 export class UserService {
@@ -11,16 +12,50 @@ export class UserService {
         @InjectRepository(User) private readonly userRepository: Repository<User>
     ) {}
 
-    async createAccount({ email, socialId, age, gender } : CreateAccountInput) : Promise<{ok: boolean; error?: string}>
-    {
+    /**
+     * 소셜 로그인 / 회원가입 로직을 처리하는 메서드
+     * @param email : 소셜 로그인 사용자의 이메일 
+     * @param socialId : 소셜 로그인 사용자의 식별값 (kakao, naver는 id, google, apple은 sub)
+     * @param vendor : 소셜 로그인 제공 회사 정보 (kakao, naver, google, apple)
+     */
+    async loginSocial(email: string, socialId: string, vendor: Vendor) {
+        
         try {
-            const exists = await this.userRepository.findOneBy({ email });
-            if (exists) {
-                return {ok: false, error: 'There is a user with that email already'};
+            const existingUser = await this.userRepository.findOneBy({ socialId });
+
+            if (existingUser) {
+                this.checkVendor(existingUser, vendor);
+
+                return {
+                    ok: true,
+                    accessToken: "TODO: JWT 토큰 정보"
+                };
             }
-            await this.userRepository.save(this.userRepository.create({email, socialId, age, gender}));
+
+            const user = await this.userRepository.save(this.userRepository.create({ 
+                email: email,
+                socialId: socialId,
+                vendor: vendor,
+                age: 20, // TODO: 나이도 입력 받아서 넣어야 함
+                gender: Gender.MAN // TODO: 성별도 입력 받아서 넣어야 함
+            }));
+
+            return {
+                ok: true,
+                accessToken: "TODO: JWT 토큰 정보"
+            };
+
         } catch (error) {
-            return {ok: false, error: "Couldn't create account"};
+            return {
+                ok: false,
+                error: error.message
+            }
+        }
+    }
+
+    checkVendor(user: User, vendor: Vendor): void {
+        if (user.vendor != vendor) {
+            throw new BadRequestException("해당 유저는 " + vendor + "의 사용자가 아닙니다."); 
         }
     }
 }
