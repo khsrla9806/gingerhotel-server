0c35009b7464735438430268eb36533560ef1fae
khsrla9806 <kimhunsope@naver.com>
2023-08-29 23:17:06 +0900

    add: Token 검증, 현재 로그인한 유저 정보 얻어오는 resolver 메서드 구현 #7

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -3,6 +3,9 @@ import { UserService } from "./users.service";
 import { User } from "./entities/user.entity";
 import { AppleSocialInput, GoogleSocialInput, KakaoSocialInput, NaverSocialInput, SocialOutput } from "./dtos/social-login-dto";
 import { Vendor } from "./entities/vendor.type";
+import { UseGuards } from "@nestjs/common";
+import { JwtAuthGuard } from "./decorator/jwt.auth.guard";
+import { LoginUserInfo } from "./decorator/current.user.decorator";
 
 @Resolver(of => User)
 export class UserResolver {
@@ -34,8 +37,16 @@ export class UserResolver {
       return this.userService.loginSocial(data.email, data.sub, Vendor.APPLE);
     }
 
+    /*
+      Client tokenTestAndCheckCurrentUser 요청
+      -> JwtAuthGuard (request 객체를 추출해서 JwtStrategy로 전달)
+      -> JwtStrategy (request 객체에서 Header의 토큰을 추출해서 검증 후 토큰의 payload를 validate에 넘겨줌)
+      -> validate (payload를 받아서 context.req.user에 반환값을 넣어줌)
+      -> LoginUserInfo (context.req.user에 있는 데이터를 뽑아서 변수에 넣어줌)
+    */
+    @UseGuards(JwtAuthGuard)
     @Query(returns => Boolean)
-    hi() {
+    tokenTestAndCheckCurrentUser(@LoginUserInfo() { userId }) {
         return true;
     }
 }
