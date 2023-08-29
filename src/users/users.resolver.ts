a0a68ee14dd07727cf6cd6681d0ace7cc3937eb0
khsrla9806 <kimhunsope@naver.com>
2023-08-29 21:15:41 +0900

    refactor: resolver 메서드에서 service 로직을 사용하도록 수정 #7

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -2,47 +2,36 @@ import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { UserService } from "./users.service";
 import { User } from "./entities/user.entity";
 import { AppleSocialInput, GoogleSocialInput, KakaoSocialInput, NaverSocialInput, SocialOutput } from "./dtos/social-login-dto";
+import { Vendor } from "./entities/vendor.type";
 
 @Resolver(of => User)
 export class UserResolver {
     constructor(
-      private readonly usersService: UserService
+      private readonly userService: UserService
     ) {}
 
     @Mutation(returns => SocialOutput)
     async socialKakao(@Args('data') data: KakaoSocialInput) {
 
-      return {
-        ok: true,
-        accessToken: "카카오 사용자"
-      };
+      return this.userService.loginSocial(data.email, data.id, Vendor.KAKAO);
     }
 
     @Mutation(returns => SocialOutput)
     async socialGoogle(@Args('data') data: GoogleSocialInput) {
-      
-      return {
-        ok: true,
-        accessToken: "구글 사용자"
-      };
+
+      return this.userService.loginSocial(data.email, data.sub, Vendor.GOOGLE);
     }
 
     @Mutation(returns => SocialOutput)
     async socialNaver(@Args('data') data: NaverSocialInput) {
-      
-      return {
-        ok: true,
-        accessToken: "네이버 사용자"
-      };
+
+      return this.userService.loginSocial(data.email, data.id, Vendor.NAVER);
     }
 
     @Mutation(returns => SocialOutput)
     async socialApple(@Args('data') data: AppleSocialInput) {
-      
-      return {
-        ok: true,
-        accessToken: "애플 사용자"
-      };
+
+      return this.userService.loginSocial(data.email, data.sub, Vendor.APPLE);
     }
 
     @Query(returns => Boolean)
