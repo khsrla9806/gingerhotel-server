de481016aced251d70fd4559fbdb996a3c388a82
khsrla9806 <kimhunsope@naver.com>
2023-10-13 01:28:10 +0900

    fix: CoreEntity의 js-joda Transformer 작동 오류로 인해 코드 재변경 #13

--- src/entities/core.entity.ts
+++ src/entities/core.entity.ts
@@ -1,20 +1,12 @@
 import { CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
-import { LocalDateTimeTransformer } from "../common/utils/local-date-time.transformer";
-import { LocalDateTime } from "@js-joda/core";
 
 export class CoreEntity {
     @PrimaryGeneratedColumn()
     id: number;
     
-    @CreateDateColumn({ 
-        type: 'timestamptz', 
-        transformer: new LocalDateTimeTransformer() 
-    })
-    creaedAt: LocalDateTime;
+    @CreateDateColumn()
+    creaedAt: Date;
     
-    @UpdateDateColumn({
-        type: 'timestamptz', 
-        transformer: new LocalDateTimeTransformer() 
-    })
-    updateAt: LocalDateTime;
+    @UpdateDateColumn()
+    updateAt: Date;
 }
