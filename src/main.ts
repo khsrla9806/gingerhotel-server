fb46a56b2fff6333567a8f469d91e0fc0222d4c0
khsrla9806 <kimhunsope@naver.com>
2023-10-13 16:39:58 +0900

    feat: swagger 문서 보안 설정 #17
    * 아무나 접근하지 못하도록 환경변수로 유저, 비밀번호 설정

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './common/swagger/setup-swagger';
+import * as expressBasicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /* swagger security start */
+  app.use(
+    ['/api-docs', ],
+    expressBasicAuth({
+      challenge: true,
+      users: {
+        [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD,
+      },
+    }),
+  );
+  /* swagger security end */
+
   setupSwagger(app);
 
   await app.listen(8080);
