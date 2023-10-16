0720db620e5e199dbcffdf2c21933f322380e525
khsrla9806 <kimhunsope@naver.com>
2023-10-16 15:14:34 +0900

    refactor: hotel window에서 hotel과 date의 Unique 제약조건 추가 #20
    * 하나의 호텔에 같은 날짜의 창문이 존재해서는 안 되기 때문에 Unuque 제약 조건 추가
    * date에 nullable false 조건을 추가

--- src/entities/hotel-window.entity.ts
+++ src/entities/hotel-window.entity.ts
@@ -3,8 +3,9 @@ import { LocalDate } from '@js-joda/core';
 import { CoreEntity } from 'src/entities/core.entity';
 import { LocalDateTransformer } from 'src/common/utils/local-date-time.transformer';
 import { Hotel } from 'src/entities/hotel.entity';
-import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, Unique } from 'typeorm';
 
+@Unique(['hotel', 'date'])
 @Entity()
 export class HotelWindow extends CoreEntity {
 
@@ -14,7 +15,8 @@ export class HotelWindow extends CoreEntity {
 
   @Column({
     type: 'date',
-    transformer: new LocalDateTransformer()
+    transformer: new LocalDateTransformer(),
+    nullable: false
   })
   date: LocalDate;
 
