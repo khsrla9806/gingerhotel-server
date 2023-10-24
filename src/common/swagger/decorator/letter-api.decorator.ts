68d71111a402a5f0ec52fefe427870f8e60cc080
khsrla9806 <kimhunsope@naver.com>
2023-10-24 18:42:36 +0900

    feat: 내 편지 조회 Swagger 적용 #26

--- src/common/swagger/decorator/letter-api.decorator.ts
+++ src/common/swagger/decorator/letter-api.decorator.ts
@@ -1,5 +1,6 @@
+import { LocalDate, LocalDateTime } from "@js-joda/core";
 import { applyDecorators } from "@nestjs/common";
-import { ApiBadRequestResponse, ApiBearerAuth, ApiBody, ApiConsumes, ApiCreatedResponse, ApiOkResponse, ApiOperation } from "@nestjs/swagger";
+import { ApiBadRequestResponse, ApiBearerAuth, ApiBody, ApiConsumes, ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiQuery } from "@nestjs/swagger";
 import { CreateLetterRequest } from "src/letters/dto/create-letter.dto";
 
 export function CreateLetterAPI() {
@@ -93,6 +94,152 @@ export function UnblockLetterAPI() {
         example: {
           success: false,
           error: '존재하지 않는 편지 정보입니다. | 내가 받은 편지만 차단 해제할 수 있습니다. | 차단 되어 있지 않은 편지입니다.'
+        }
+      }
+    }),
+    ApiBearerAuth('Authorization')
+  );
+}
+
+export function GetLettersAPI() {
+  return applyDecorators(
+    ApiOperation({ summary: '내 편지함 조회 (편지 + 답장)', description: '선택한 날짜에(yyyy-MM-dd)에 해당하는 창문에 도착한 편지와 답장을 조회합니다.' }),
+    ApiQuery({
+      name: 'date',
+      type: 'yyyy-MM-dd',
+      example: '2023-12-01',
+      description: '조회하려는 편지함(창문)의 날짜를 입력 (Ex. 1번 창문의 경우 2023-12-01)'
+    }),
+    ApiOkResponse({
+      description: '편지함 조회 성공',
+      schema: {
+        example: {
+          success: true,
+          letters: [
+              {
+                  id: 1,
+                  createdAt: "2023-10-24T17:38:02.317",
+                  senderNickname: "삼번유저",
+                  content: "안녕~~친구야~~~",
+                  isBlocked: false,
+                  imageUrl: "저장된 image URL"
+              },
+              {
+                  id: 2,
+                  createdAt: "2023-10-24T17:38:02.317",
+                  senderNickname: "삼번유저",
+                  content: "안녕~~친구야~~~",
+                  isBlocked: false,
+                  imageUrl: "저장된 image URL"
+              },
+              {
+                  id: 3,
+                  createdAt: "2023-10-24T17:38:02.317",
+                  senderNickname: "삼번유저",
+                  content: "안녕~~친구야~~~",
+                  isBlocked: false,
+                  imageUrl: "저장된 image URL"
+              },
+              {
+                  id: 4,
+                  createdAt: "2023-10-24T17:38:02.317",
+                  senderNickname: "삼번유저",
+                  content: "안녕~~친구야~~~",
+                  isBlocked: false,
+                  imageUrl: "저장된 image URL"
+              },
+              {
+                  id: 5,
+                  createdAt: "2023-10-24T17:38:02.317",
+                  senderNickname: "삼번유저",
+                  content: "안녕~~친구야~~~",
+                  isBlocked: false,
+                  imageUrl: "저장된 image URL"
+              },
+              {
+                  id: 6,
+                  createdAt: "2023-10-24T17:38:02.317",
+                  senderNickname: "삼번유저",
+                  content: "안녕~~친구야~~~",
+                  isBlocked: false,
+                  imageUrl: "저장된 image URL"
+              },
+              {
+                  id: 7,
+                  createdAt: "2023-10-24T17:38:02.317",
+                  senderNickname: "삼번유저",
+                  content: "안녕~~친구야~~~",
+                  isBlocked: false,
+                  imageUrl: "저장된 image URL"
+              },
+              {
+                  id: 8,
+                  createdAt: "2023-10-24T17:38:02.317",
+                  senderNickname: "삼번유저",
+                  content: "안녕~~친구야~~~",
+                  isBlocked: false,
+                  imageUrl: "저장된 image URL"
+              },
+              {
+                  id: 9,
+                  createdAt: "2023-10-24T17:38:02.317",
+                  senderNickname: "삼번유저",
+                  content: "안녕~~친구야~~~",
+                  isBlocked: false,
+                  imageUrl: "저장된 image URL"
+              },
+              {
+                  id: 10,
+                  createdAt: "2023-10-24T17:38:02.317",
+                  senderNickname: "삼번유저",
+                  content: "안녕~~친구야~~~",
+                  isBlocked: false,
+                  imageUrl: "저장된 image URL"
+              }
+          ],
+          "replies": [
+              {
+                  id: 5,
+                  createdAt: "2023-10-24T18:22:22.543",
+                  content: "9번 편지에 3번 유저가 보내는 답장(4)",
+                  isBlocked: false,
+                  imageUrl: "저장된 image URL",
+                  senderNickname: "삼번유저"
+              },
+              {
+                  id: 4,
+                  createdAt: "2023-10-24T17:38:53.183",
+                  content: "9번 편지에 3번 유저가 보내는 답장(3)",
+                  isBlocked: false,
+                  imageUrl: "저장된 image URL",
+                  senderNickname: "삼번유저"
+              },
+              {
+                  id: 3,
+                  createdAt: "2023-10-24T17:38:15.780",
+                  content: "9번 편지에 3번 유저가 보내는 답장(2)",
+                  isBlocked: false,
+                  imageUrl: "저장된 image URL",
+                  senderNickname: "삼번유저"
+              },
+              {
+                  id: 2,
+                  createdAt: "2023-10-24T17:38:02.317",
+                  content: "9번 편지에 3번 유저가 보내는 답장(1)",
+                  isBlocked: false,
+                  imageUrl: "저장된 image URL",
+                  senderNickname: "삼번유저"
+              }
+          ]
+      }
+      }
+    }),
+    ApiBadRequestResponse({
+      description: '편지함 조회 실패',
+      schema: {
+        example: {
+          success: false,
+          error: '존재하지 않는 호텔 정보입니다. | 내 호텔의 편지만 확인할 수 있습니다. | 2023-12-01에 받은 편지가 존재하지 않습니다.'
         }
       }
     }),
--- src/letters/letters.controller.ts
+++ src/letters/letters.controller.ts
@@ -6,7 +6,7 @@ import { LoginMember } from 'src/auth/decorator/login-member.decorator';
 import { Member } from 'src/entities/member.entity';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { CreateLetterRequest } from './dto/create-letter.dto';
-import { BlockLetterAPI, CreateLetterAPI, DeleteLetterAPI, UnblockLetterAPI } from 'src/common/swagger/decorator/letter-api.decorator';
+import { BlockLetterAPI, CreateLetterAPI, DeleteLetterAPI, GetLettersAPI, UnblockLetterAPI } from 'src/common/swagger/decorator/letter-api.decorator';
 import { LocalDate } from '@js-joda/core';
 import { StringToLocalDateValidationPipe } from './pipes/string-to-local-date.validation.pipe';
 
@@ -63,6 +63,7 @@ export class LettersController {
 
   @Get('/hotel/:hotelId')
   @UseGuards(AuthGuard())
+  @GetLettersAPI()
   async getLetters(
     @Param('hotelId', ParseIntPipe) hotelId: number,
     @Query('date', StringToLocalDateValidationPipe) date: LocalDate,
