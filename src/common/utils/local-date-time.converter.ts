dd4e9b80ee8511cda5fb649bfe21bcbd2f971143
khsrla9806 <kimhunsope@naver.com>
2023-10-25 17:59:47 +0900

    feat: Date 타입을 LocalDateTime으로 변경하는 메서드 구현 #30
    * 단일 객체에서도 사용할 일이 있기 떄문에 메서드로 분리

--- src/common/utils/local-date-time.converter.ts
+++ src/common/utils/local-date-time.converter.ts
@@ -1,7 +1,16 @@
-import { nativeJs } from "@js-joda/core";
+import { LocalDateTime, nativeJs } from "@js-joda/core";
+import { InternalServerErrorException } from "@nestjs/common";
 
 export class LocalDateTimeConverter {
-  public static convertCreatedAtToLocalDateTimeInList(list: any) {
-    list.map((data) => { data.createdAt = nativeJs(data.createdAt).toLocalDateTime() });
+  public static convertCreatedAtToLocalDateTimeInList(list: any[]) {
+    list.map((data) => { data.createdAt = this.convertDateToLocalDateTime(data.createdAt) });
+  }
+
+  public static convertDateToLocalDateTime(date: Date): LocalDateTime {
+    try {
+      return nativeJs(date).toLocalDateTime();
+    } catch (e) {
+      throw new InternalServerErrorException('[convertDateToLocalDateTime] 잘못된 Date 형태입니다.');
+    }
   }
 }
