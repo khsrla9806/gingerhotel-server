3a9136018f6e88ff47dde0387b6229bc13198b85
Khsrla9806 <kimhunsope@naver.com>
2023-11-10 21:52:50 +0900

    refactor: 알림 페이지 조회 Swagger 모든 내용 포함하여 수정 #82

--- src/common/swagger/decorator/notification-api.decorator.ts
+++ src/common/swagger/decorator/notification-api.decorator.ts
@@ -10,112 +10,116 @@ export function GetNotificationsAPI() {
       schema: {
         example: {
           "data": [
-              {
-                  "id": 25000072,
-                  "createdAt": "2023-11-10T00:46:55.375",
-                  "type": "FEEK_REQUEST",
-                  "typeData": {
-                      "feekId": 2
-                  },
-                  "message": "4번섭님께서 엿보기를 요청했어요!",
-                  "isChecked": true
+            {
+              "id": 31000120,
+              "createdAt": "2023-11-10T21:36:27.473",
+              "type": "FEEK_REJECT",
+              "typeData": {
+                  "hotelId": 4,
+                  "date": "2023-11-10"
               },
-              {
-                  "id": 25000071,
-                  "createdAt": "2023-11-10T00:46:55.375",
-                  "type": "FEEK_REQUEST",
-                  "typeData": {
-                      "feekId": 2
-                  },
-                  "message": "4번섭님께서 엿보기를 요청했어요!",
-                  "isChecked": true
+              "message": "너 102만 유저네? 대박님께서 엿보기를 거절했어요 :(",
+              "isChecked": false
+            },
+            {
+              "id": 31000119,
+              "createdAt": "2023-11-10T21:36:01.598",
+              "type": "FEEK_ACCEPT",
+              "typeData": {
+                  "hotelId": 4,
+                  "date": "2023-11-10"
               },
-              {
-                  "id": 25000070,
-                  "createdAt": "2023-11-10T00:46:55.375",
-                  "type": "FEEK_REQUEST",
-                  "typeData": {
-                      "feekId": 2
-                  },
-                  "message": "4번섭님께서 엿보기를 요청했어요!",
-                  "isChecked": true
+              "message": "너 102만 유저네? 대박님께서 엿보기를 수락했어요!",
+              "isChecked": false
+            },
+            {
+              "id": 31000118,
+              "createdAt": "2023-11-10T21:32:48.838",
+              "type": "WINDOW_OPEN",
+              "typeData": {
+                  "hotelId": 4,
+                  "date": "2023-11-10"
               },
-              {
-                  "id": 25000069,
-                  "createdAt": "2023-11-10T00:46:55.375",
-                  "type": "FEEK_REQUEST",
-                  "typeData": {
-                      "feekId": 2
-                  },
-                  "message": "4번섭님께서 엿보기를 요청했어요!",
-                  "isChecked": true
+              "message": "오늘의 창문이 열렸어요! 확인하러 갈까요?",
+              "isChecked": false
+            },
+            {
+              "id": 31000116,
+              "createdAt": "2023-11-10T21:31:38.557",
+              "type": "LETTER",
+              "typeData": {
+                  "hotelId": 4,
+                  "date": "2023-11-10"
               },
-              {
-                  "id": 25000068,
-                  "createdAt": "2023-11-10T00:46:55.375",
-                  "type": "FEEK_REQUEST",
-                  "typeData": {
-                      "feekId": 2
-                  },
-                  "message": "4번섭님께서 엿보기를 요청했어요!",
-                  "isChecked": true
+              "message": "두근두근! 새 편지 도착!",
+              "isChecked": false
+            },
+            {
+              "id": 31000091,
+              "createdAt": "2023-11-10T21:30:25.568",
+              "type": "REPLY",
+              "typeData": {
+                  "letterId": 19000020
               },
-              {
-                  "id": 25000067,
-                  "createdAt": "2023-11-10T00:46:55.375",
-                  "type": "FEEK_REQUEST",
-                  "typeData": {
-                      "feekId": 2
-                  },
-                  "message": "4번섭님께서 엿보기를 요청했어요!",
-                  "isChecked": true
+              "message": "편지함에 답장이 도착했어요!",
+              "isChecked": false
+            },
+            {
+              "id": 25000071,
+              "createdAt": "2023-11-10T00:46:55.375",
+              "type": "FEEK_REQUEST",
+              "typeData": {
+                  "feekId": 2
               },
-              {
-                  "id": 15000056,
-                  "createdAt": "2023-11-10T00:46:55.375",
-                  "type": "FEEK_REQUEST",
-                  "typeData": {
-                      "feekId": 2
-                  },
-                  "message": "4번섭님께서 엿보기를 요청했어요!",
-                  "isChecked": true
+              "message": "4번섭님께서 엿보기를 요청했어요!",
+              "isChecked": true
+            },
+            {
+              "id": 25000069,
+              "createdAt": "2023-11-10T00:46:55.375",
+              "type": "FEEK_REQUEST",
+              "typeData": {
+                  "feekId": 2
               },
-              {
-                  "id": 15000055,
-                  "createdAt": "2023-11-10T00:46:55.375",
-                  "type": "FEEK_REQUEST",
-                  "typeData": {
-                      "feekId": 2
-                  },
-                  "message": "4번섭님께서 엿보기를 요청했어요!",
-                  "isChecked": true
+              "message": "4번섭님께서 엿보기를 요청했어요!",
+              "isChecked": true
+            },
+            {
+              "id": 25000068,
+              "createdAt": "2023-11-10T00:46:55.375",
+              "type": "FEEK_REQUEST",
+              "typeData": {
+                  "feekId": 2
               },
-              {
-                  "id": 15000054,
-                  "createdAt": "2023-11-10T00:46:55.375",
-                  "type": "FEEK_REQUEST",
-                  "typeData": {
-                      "feekId": 2
-                  },
-                  "message": "4번섭님께서 엿보기를 요청했어요!",
-                  "isChecked": true
+              "message": "4번섭님께서 엿보기를 요청했어요!",
+              "isChecked": true
+            },
+            {
+              "id": 25000067,
+              "createdAt": "2023-11-10T00:46:55.375",
+              "type": "FEEK_REQUEST",
+              "typeData": {
+                  "feekId": 2
               },
-              {
-                  "id": 15000053,
-                  "createdAt": "2023-11-10T00:46:55.375",
-                  "type": "FEEK_REQUEST",
-                  "typeData": {
-                      "feekId": 2
-                  },
-                  "message": "4번섭님께서 엿보기를 요청했어요!",
-                  "isChecked": true
-              }
+              "message": "4번섭님께서 엿보기를 요청했어요!",
+              "isChecked": true
+            },
+            {
+              "id": 15000055,
+              "createdAt": "2023-11-10T00:46:55.375",
+              "type": "FEEK_REQUEST",
+              "typeData": {
+                  "feekId": 2
+              },
+              "message": "4번섭님께서 엿보기를 요청했어요!",
+              "isChecked": true
+            }
           ],
           "meta": {
-              "total": "40",
+              "total": 40,
               "size": 10,
               "hasNextData": true,
-              "cursor": 15000053
+              "cursor": 15000055
           }
         }
       }
