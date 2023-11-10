6004639466ac747eb6776adae21ce07f1e9ae7f0
Khsrla9806 <kimhunsope@naver.com>
2023-11-10 16:45:56 +0900

    feat: 알림 페이지 cursor pagination service 로직 구현 #79

--- src/notifications/service/notifications.service.ts
+++ src/notifications/service/notifications.service.ts
@@ -2,8 +2,12 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Member } from 'src/entities/member.entity';
 import { NotificationHistory } from 'src/entities/notification-history.entity';
-import { Repository } from 'typeorm';
+import { Repository, SelectQueryBuilder } from 'typeorm';
 import { GetNotificationsResponse } from '../dto/get-notifications.dto';
+import { CursorPageOptionDTO } from '../../common/dto/cursor-page-option.dto';
+import { Order } from '../../common/dto/cursor-page-order.enum';
+import { CursorPageMetaDTO } from 'src/common/dto/cursor-page-meta.dto';
+import { CursorPageDTO } from 'src/common/dto/cursor-page.dto';
 
 @Injectable()
 export class NotificationsService {
@@ -15,19 +19,46 @@ export class NotificationsService {
   /**
    * 내 알림 목록 조회
    */
-  async getNotifications(loginMember: Member) {
+  async getNotifications(
+    loginMember: Member, { sort, size, cursorId }: CursorPageOptionDTO): Promise<CursorPageDTO<GetNotificationsResponse>> {
     try {
-      const notificationHistories: NotificationHistory[] = await this.notificationHistoryRepository
+      const [{ total }] = await this.notificationHistoryRepository.query(
+        `SELECT COUNT(*) as total FROM notification_history WHERE member_id = $1`,
+        [loginMember.id]
+      );
+      
+      const notificationHistoriesQueryBuilder: SelectQueryBuilder<NotificationHistory> = this.notificationHistoryRepository
         .createQueryBuilder('notificationHistory')
-        .innerJoin('notificationHistory.member', 'member', 'member.id = :memberId', { memberId: loginMember.id })
-        .orderBy('notificationHistory.id', 'DESC')
+        .innerJoin('notificationHistory.member', 'member', 'member.id = :memberId', { memberId: loginMember.id });
+      
+      if (cursorId) {
+        notificationHistoriesQueryBuilder.where('notificationHistory.id < :cursorId', { cursorId: cursorId });
+      }
+
+      const notificationHistories: NotificationHistory[] = await notificationHistoriesQueryBuilder
+        .orderBy('notificationHistory.id', sort === Order.DESC ? 'DESC' : 'ASC')
+        .limit(size)
         .getMany();
 
-      const notificationResponse = notificationHistories.map((notificationHistory): GetNotificationsResponse => {
-        return GetNotificationsResponse.from(notificationHistory);
-      })
+      const notificationResponse: GetNotificationsResponse[] = notificationHistories
+        .map((notificationHistory): GetNotificationsResponse => {
+          return GetNotificationsResponse.from(notificationHistory);
+        });
 
-      return notificationResponse;
+      const isLastPage: boolean = total <= size;
+      let hasNexPage: boolean = true;
+      let cursor: number;
+
+      if (isLastPage || notificationResponse.length < size) {
+        hasNexPage = false;
+        cursor = null;
+      } else {
+        cursor = notificationResponse[notificationResponse.length - 1].getId();
+      }
+
+      const cursorPageMetaDTO: CursorPageMetaDTO = new CursorPageMetaDTO(total, size, hasNexPage, cursor);
+
+      return new CursorPageDTO(notificationResponse, cursorPageMetaDTO);
     } catch (error) {
       throw error;
     }
