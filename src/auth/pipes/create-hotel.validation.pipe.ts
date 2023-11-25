824a4b4e8eb35dc519fd1b45620fcf208df9dffb
Hoonsub Kim <kimhunsope@naver.com>
2023-11-26 01:27:51 +0900

    feat: add validation in create hotel #99 (#100)

--- src/auth/pipes/create-hotel.validation.pipe.ts
+++ src/auth/pipes/create-hotel.validation.pipe.ts
@@ -6,15 +6,44 @@ export class CreateHotelValidationPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
       try {
         if (value.birthDate) {
-          const birthDate = value.birthDate as string;
-          const parsedBirthDate = LocalDate.parse(birthDate);
-          value.birthDate = parsedBirthDate;
+          try {
+            const birthDate = value.birthDate as string;
+            const parsedBirthDate = LocalDate.parse(birthDate);
+            value.birthDate = parsedBirthDate;
+          } catch (error) {
+            throw new BadRequestException(`잘못된 날짜 형식입니다. (input : ${value.birthDate})`);
+          }
+        }
+
+        if (!value.nickname) {
+          throw new BadRequestException('닉네임은 필수값입니다.');
+        }
+
+        if (typeof value.nickname !== "string") {
+          throw new BadRequestException('nickname must be string.')
+        }
+        value.nickanme = value.nickname.trim();
+
+        if (value.nickname.length > 7) {
+          throw new BadRequestException('닉네임은 최대 7글자입니다.');
+        }
+
+        if (!value.description) {
+          throw new BadRequestException('호텔 설명은 필수값입니다.');
+        }
+        if (typeof value.description !== "string") {
+          throw new BadRequestException('nickname must be string.')
+        }
+        value.description = value.description.trim();
+
+        if (value.description.length > 25) {
+          throw new BadRequestException('호텔 설명은 최대 25글자입니다.');
         }
 
         return value;
         
       } catch (error) {
-        throw new BadRequestException(`잘못된 날짜 형식입니다. (입력된 값 : ${value.birthDate})`);
+        throw error;
       }
   }
 }
