95cc54458958cc3f70dddcbc3095608ccb80f708
Khsrla9806 <kimhunsope@naver.com>
2023-11-26 23:18:36 +0900

    feat: hotel decorator 추가 #105

---
+++ src/entities/domain/hotel-decorator.type.ts
@@ -0,0 +1,27 @@
+
+// 건물 장식 (3개)
+export enum BuildingDecorator {
+    buildingDeco01 = 'buildingDeco01',
+    buildingDeco02 = 'buildingDeco02',
+    buildingDeco03 = 'buildingDeco03'
+}
+
+// 마당 장식 (3개)
+export enum GardenDecorator {
+    gardenDeco01 = 'gardenDeco01',
+    gardenDeco02 = 'gardenDeco02',
+    gardenDeco03 = 'gardenDeco03'
+}
+
+// 창문 장식 (2개)
+export enum WindowDecorator {
+    windowDeco01 = 'windowDeco01',
+    windowDeco02 = 'windowDeco02'
+}
+
+// 뒷배경 (3개)
+export enum Background {
+    background01 = 'background01',
+    background02 = 'background02',
+    background03 = 'background03'
+}
